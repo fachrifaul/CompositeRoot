49ad134f6422d203494f1c6f4b1a1ff5778d1266
Fachri Febrian <fachripaul@gmail.com>
2021-03-05 17:38:04 +0700

    remove

--- Pods/Target Support Files/Pods-CompositeRoot-CompositeRootUITests/Pods-CompositeRoot-CompositeRootUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CompositeRoot_CompositeRootUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CompositeRoot_CompositeRootUITests
-@end
--- Pods/Target Support Files/Pods-CompositeRootTests/Pods-CompositeRootTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CompositeRootTests : NSObject
-@end
-@implementation PodsDummy_Pods_CompositeRootTests
-@end
